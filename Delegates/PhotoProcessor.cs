c73c0f755dac2a488265456edbea463ce8788443
Jun <Jun@JUN-PC>
2017-01-25 00:10:52 -0500

    add code

---
+++ Delegates/PhotoProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        //public delegate void PhotoFilterHandler(Photo photo);
+
+        //public void Process(string path, PhotoFilterHandler filterHandler)
+        //{
+        //    var photo = Photo.Load(path);
+        //    filterHandler(photo);
+        //    photo.Save();
+        //}
+        //Use Func<Params,ReturnOut> 
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            var photo = Photo.Load(path);
+            filterHandler(photo);
+            photo.Save();
+        }
+    }
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -10,6 +10,20 @@ namespace Delegates
     {
         static void Main(string[] args)
         {
+            var photoProcessor = new PhotoProcessor();
+            var filter = new PhotoFilters();
+
+            //PhotoProcessor.PhotoFilterHandler filterHandler = filter.ApplyBrightness;
+            Action<Photo> filterHandler = filter.ApplyBrightness;
+            filterHandler += filter.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+            photoProcessor.Process("photo.jpg", filterHandler);
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Remove red eye");
         }
     }
 }
